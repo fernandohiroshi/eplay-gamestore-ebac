1dae4a8faf47b1b1f310b433a95332ba34da0653
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-02-29 01:46:51 -0300

    Creating a modal system, image zoom settings

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,80 @@
+import Section from '../Section'
+import { Items, Item, Action, Modal, ModalContent } from './style'
+
+import zelda from '../../assets/images/zelda.png'
+import spiderman from '../../assets/images/spider_man.png'
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import close from '../../assets/images/fechar.png'
+
+type GalleryItem = {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: zelda
+  },
+  {
+    type: 'image',
+    url: spiderman
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ?si=jqMBlTpG_-lfGY6p'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  return (
+    <>
+      <Section title="Gallery" background="black">
+        <Items>
+          {mock.map((media, index) => (
+            <Item key={media.url}>
+              <img
+                src={getMediaCover(media)}
+                alt={`Media ${index + 1} - ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Click to maximize the media"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={close} alt="close image" />
+          </header>
+          <img src={spiderman} />
+        </ModalContent>
+        <div className="overlay"></div>
+      </Modal>
+    </>
+  )
+}
+
+export default Gallery
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,8 @@
 import { useParams } from 'react-router-dom'
 import Hero from '../../components/Hero'
+import Section from '../../components/Section'
+import Gallery from '../../components/Gallery'
+import residentEvil from '../../assets/images/resident.png'
 
 const Product = () => {
   const { id } = useParams()
@@ -7,6 +10,32 @@ const Product = () => {
   return (
     <>
       <Hero />
+      <Section title="About the game" background="black">
+        <p>
+          Hogwarts Legacy is an action RPG video game developed by Avalanche
+          Software and published by Warner Bros. Interactive Entertainment under
+          the Portkey Games label. The game is set in the late 19th century in
+          the Harry Potter universe, a century before the events narrated in
+          J.K. Rowling&apos;s books. The player controls a student enrolled at
+          Hogwarts School of Witchcraft and Wizardry who learns to wield a
+          variety of magical skills and objects. With the help of classmates and
+          professors, the protagonist embarks on a journey to uncover an ancient
+          secret long hidden in the magical world.
+        </p>
+      </Section>
+      <Section title="More details." background="gray">
+        <p>
+          <b>Plataforma:</b> PlayStation 5 <br />
+          <b>Desenvolvedor:</b> Avalanche Software <br />
+          <b>Editora:</b> Portkey Games, subsidiária da Warner Bros. Interactive
+          Entertainment <br />
+          <b>Idiomas:</b> O jogo oferece suporte a diversos idiomas, incluindo
+          inglês, espanhol, francês, alemão, italiano, português, entre outros.
+          As opções de áudio e legendas podem ser ajustadas nas configurações do
+          jogo.
+        </p>
+      </Section>
+      <Gallery defaultCover={residentEvil} name="Game" />
     </>
   )
 }
